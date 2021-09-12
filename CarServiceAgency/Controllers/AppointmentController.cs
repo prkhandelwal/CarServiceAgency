de0396671f015dc260100b41e7b61756edd85ec9
Pratik Khandelwal <prkhandelwal@hotmail.com>
2021-09-12 20:07:26 +0530

    Add project files.

---
+++ CarServiceAgency/Controllers/AppointmentController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarServiceAgency.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppointmentController : ControllerBase
+    {
+        private readonly ILogger<AppointmentController> _logger;
+
+        public AppointmentController(ILogger<AppointmentController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            return new StatusCodeResult(StatusCodes.Status200OK);
+        }
+    }
+}
